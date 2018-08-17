6ce434780f02600d49c94d8547c5232cace63f30
macsj200 <maxjohansen@berkeley.edu>
2018-08-16 19:01:29 -0600

    more ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CongresspersonDetailComponent } from './congressperson-detail/congressperson-detail.component';
+import { CongresspersonListComponent } from './congressperson-list/congressperson-list.component';
+import { CongresspersonSearchComponent } from './congressperson-search/congressperson-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CongresspersonDetailComponent
+    CongresspersonDetailComponent,
+    CongresspersonListComponent,
+    CongresspersonSearchComponent
   ],
   imports: [
     BrowserModule
